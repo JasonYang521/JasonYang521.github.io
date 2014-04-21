ad86afd4589a18e836b97de1e61048dd29816cd2
Jason Yang <mr.jasonyang@gmail.com>
2014-04-21 04:45:16 -0700

    debugging

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,12 +4,7 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', [function() {
-				<div ng-controller="Hello">
-			<p>The ID is {{greeting.items[0].owner}}</p>
-				<ul ng-repeat="item in greeting.items">
-			<li>The Value and Key  is {{item.owner.reputation}}</li>
-		</ul>
-		</div>
+
   }])
   .controller('MyCtrl2', [function() {
 	
