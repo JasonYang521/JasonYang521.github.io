285e6ffcc9f8e1f8062af9790666dac366587898
Jason Yang <mr.jasonyang@gmail.com>
2014-04-23 02:53:14 -0700

    Completed Final View
    Now for Beautification and final touches

--- app/js/filters.js
+++ app/js/filters.js
@@ -3,11 +3,7 @@
 /* Filters */
 
 angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  }]).filter('unsafe', ['$sce', function ($sce) {
+filter('unsafe', ['$sce', function ($sce) {
     return function (val) {
         return $sce.trustAsHtml(val);
     };
